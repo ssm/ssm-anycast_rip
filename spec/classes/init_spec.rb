9c41c564f0915b5de038815b2d4ea2c99d8f0716
Stig Sandbeck Mathisen <ssm@fnord.no>
2016-10-27 13:37:12 +0200

    Support and test on multiple operating systems

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 describe 'anycast_rip' do
 
-  context 'with default values for all parameters' do
-    it { is_expected.to contain_class('anycast_rip') }
-    it { is_expected.to contain_service('bird') }
-    it { is_expected.to contain_service('bird6') }
-    it { is_expected.to contain_file('/etc/bird/bird.conf') }
-    it { is_expected.to contain_file('/etc/bird/bird6.conf') }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      context 'with default values for all parameters' do
+        it { is_expected.to contain_class('anycast_rip') }
+        it { is_expected.to contain_service('bird') }
+        it { is_expected.to contain_service('bird6') }
+        it { is_expected.to contain_file('/etc/bird/bird.conf') }
+        it { is_expected.to contain_file('/etc/bird/bird6.conf') }
+      end
 
-  context 'only IPv6' do
-    let(:params) { { instances: ['bird6'] } }
-    it { is_expected.to contain_class('anycast_rip') }
-    it { is_expected.not_to contain_service('bird') }
-    it { is_expected.to contain_service('bird6') }
-    it { is_expected.not_to contain_file('/etc/bird/bird.conf') }
-    it { is_expected.to contain_file('/etc/bird/bird6.conf') }
+      context 'only IPv6' do
+        let(:params) { { instances: ['bird6'] } }
+        it { is_expected.to contain_class('anycast_rip') }
+        it { is_expected.not_to contain_service('bird') }
+        it { is_expected.to contain_service('bird6') }
+        it { is_expected.not_to contain_file('/etc/bird/bird.conf') }
+        it { is_expected.to contain_file('/etc/bird/bird6.conf') }
+      end
+    end
   end
-
 end
