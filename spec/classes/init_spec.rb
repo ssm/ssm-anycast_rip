e4eb1c4a9b9e3b325bd3a80f1c86d70a9b88ffee
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-09-27 12:31:42 +0200

    Update tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,9 +1,14 @@
-require 'spec_helper'
-describe 'anycast_rip' do
+# frozen_string_literal: true
 
-  on_supported_os.each do |os, facts|
+require 'spec_helper'
+
+describe 'anycast_rip' do
+  on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:facts) { os_facts }
+
       context 'with default values for all parameters' do
+        it { is_expected.to compile }
         it { is_expected.to contain_class('anycast_rip') }
         it { is_expected.to contain_service('bird') }
         it { is_expected.to contain_service('bird6') }
@@ -13,6 +18,8 @@ describe 'anycast_rip' do
 
       context 'only IPv6' do
         let(:params) { { instances: ['bird6'] } }
+
+        it { is_expected.to compile }
         it { is_expected.to contain_class('anycast_rip') }
         it { is_expected.not_to contain_service('bird') }
         it { is_expected.to contain_service('bird6') }
