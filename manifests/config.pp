da048f5f30671e36db80af44e3e08a264c1d4902
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-09-27 13:34:28 +0200

    Move optional parameter down

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,10 @@
 # Configure bird and bird6 instances
 # @api private
 class anycast_rip::config (
-  Array[Enum['bird', 'bird6']] $instances,
   Stdlib::Absolutepath $config_dir,
   String $config_file_owner,
   String $config_file_group,
+  Array[Enum['bird', 'bird6']] $instances,
   Array[Variant[Stdlib::IP::Address::V6::CIDR, Stdlib::IP::Address::V4::CIDR]] $network_prefixes = [],
   String $network_interface,
   Optional[String] $auth_password,
