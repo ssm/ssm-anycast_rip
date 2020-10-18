0ffb4365c75a336970ed17037846fd0f375e1f6f
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-10-18 13:18:23 +0200

    Add tests for anycast_rip::config

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'anycast_rip::config' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          config_dir: '/test',
+          config_file_owner: 'root',
+          config_file_group: 'bird',
+          network_interface: 'test0',
+          auth_password: 'test secret',
+          instances: ['bird', 'bird6'],
+          network_prefixes: [
+            '192.0.2.192/25',
+            '2001:db8:1::/64',
+          ],
+        }
+      end
+
+      it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_file('/test/bird.conf')
+          .with_content(%r{192\.0\.2\.192/25\+})
+          .without_content(%r{2001:db8:1::/64\+})
+          .with_content(%r{interface "test0";})
+          .with_content(%r{password "test secret";})
+      }
+      it {
+        is_expected.to contain_file('/test/bird6.conf')
+          .with_content(%r{2001:db8:1::/64\+})
+          .without_content(%r{192\.0\.2\.192/25\+})
+          .with_content(%r{interface "test0";})
+          .with_content(%r{password "test secret";})
+      }
+    end
+  end
+end
