0300cd2b369c5ec8e0c7b84cd7a2d113575ecb61
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-10-18 08:29:42 +0200

    Update tests with config paths

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,13 +7,24 @@ describe 'anycast_rip' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      before(:each) do
+        case facts[:os]['family']
+        when 'RedHat'
+          @bird_conf = '/etc/bird.conf'
+          @bird6_conf = '/etc/bird6.conf'
+        else
+          @bird_conf = '/etc/bird/bird.conf'
+          @bird6_conf = '/etc/bird/bird6.conf'
+        end
+      end
+
       context 'with default values for all parameters' do
         it { is_expected.to compile }
         it { is_expected.to contain_class('anycast_rip') }
         it { is_expected.to contain_service('bird') }
         it { is_expected.to contain_service('bird6') }
-        it { is_expected.to contain_file('/etc/bird/bird.conf') }
-        it { is_expected.to contain_file('/etc/bird/bird6.conf') }
+        it { is_expected.to contain_file(@bird_conf) }
+        it { is_expected.to contain_file(@bird6_conf) }
       end
 
       context 'only IPv6' do
@@ -23,8 +34,8 @@ describe 'anycast_rip' do
         it { is_expected.to contain_class('anycast_rip') }
         it { is_expected.not_to contain_service('bird') }
         it { is_expected.to contain_service('bird6') }
-        it { is_expected.not_to contain_file('/etc/bird/bird.conf') }
-        it { is_expected.to contain_file('/etc/bird/bird6.conf') }
+        it { is_expected.not_to contain_file(@bird_conf) }
+        it { is_expected.to contain_file(@bird6_conf) }
       end
     end
   end
