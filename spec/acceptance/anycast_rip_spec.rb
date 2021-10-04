3308f2f43d734be390b82e48e9de178452dcf9a5
Stig Sandbeck Mathisen <ssm@fnord.no>
2021-10-04 10:55:21 +0200

    acceptance tests: install epel on redhat

--- spec/acceptance/anycast_rip_spec.rb
+++ spec/acceptance/anycast_rip_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper_acceptance'
 
 pp_default = <<-PUPPETCODE
+  if $facts['os']['family'] == 'RedHat' {
+    package { 'epel-release':
+      ensure => installed,
+      before => Class['anycast_rip'],
+    }
+  }
   include anycast_rip
 PUPPETCODE
 
